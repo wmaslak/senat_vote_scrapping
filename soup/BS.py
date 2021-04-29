1c1a0bbe4fa72f21440b8c1ae5ce41b755094e21
Wojciech Maślakiewicz <wmaslakiewicz@gmail.com>
2021-04-29 18:45:07 +0200

    can now scrap a single session page for name, date and links to votings

--- soup/BS.py
+++ soup/BS.py
@@ -20,8 +20,8 @@ voting_id = 10100
 voting_url = f'https://www.senat.gov.pl/prace/posiedzenia/glosowanie-drukuj,{voting_id}.html'
 #session_day_url =
 
-day_no = 1
-session_id = 501
+day_no = 2
+session_id = 502
 url = f"https://www.senat.gov.pl/prace/posiedzenia/przebieg,{session_id},{day_no},glosowania.html"
 print(url)
 
@@ -38,13 +38,15 @@ def scrap_single_voting(voting_url):
 	try:
 		bs = get_url_content(voting_url)
 		senators_raw = bs.find_all('div',{'class' : 'senator'})
-		senators_clean = [[tag.text.strip('\t\n ') for tag in senator.findChildren()] for senator in senators_raw]
-		df = pd.DataFrame(senators_clean)
+		senators = [[tag.text.strip('\t\n ') for tag in senator.findChildren()] for senator in senators_raw]
+		df = pd.DataFrame(senators)
 		df.columns = ["Senator","Vote"]
 		return(df)
 	except:
 		print(f"Problem! Voting's url:\n {url}" )
 		
+
+
 def scrap_day_of_session(session_day_url):
 	bs = get_url_content(session_day_url)
 	# get session title
@@ -57,8 +59,27 @@ def scrap_day_of_session(session_day_url):
 	
 	
 	
-	print(session_title)
-	return(-1)
+	if bs.find('div', {'class' : "table-content"}):
+		votings_table = bs.find('div', {'class' : "table-content"}).find_all('div', {'class' : "row"})
+		votings_rows = [row.find_all('div') for row in votings_table]
+		
+		# skip every second "row" since its actually part of the same row (possibly optimize this)
+		votings_extracted = [[vote[i] for i in [0,1,-1]] for k,vote in enumerate(votings_rows) if k%2==0]
+		
+		# list of such lists: [# of vote in a day, title, subscript, link to results]
+		votings = [[vote[0].get_text(strip = True), vote[1].findChildren()[0].text ,vote[1].p.i.text if vote[1].p else 'NA' ,
+		 vote[2].a['href'] ] for vote in votings_extracted]
+		
+		#print(votings_extracted)
+		df = pd.DataFrame(votings)
+		df.columns = ["No. of voting","Title","Subscript","Link"]
+		df["Session"] = session_title
+		df["Dates"] = session_dates
+		return(df)
+	else:
+		return('zonk')
+	
+	
 	
 
 print(scrap_day_of_session(url))
